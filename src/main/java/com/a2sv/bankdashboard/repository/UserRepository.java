7904eb8658fd21ed1f3699d7c6463f4fca9b84e2
solomon Abate <solomonabate18@gmail.com>
2024-08-10 11:52:14 +0300

    Add repository

---
+++ src/main/java/com/a2sv/bankdashboard/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.a2sv.bankdashboard.repository;
+
+import com.a2sv.bankdashboard.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
+}
