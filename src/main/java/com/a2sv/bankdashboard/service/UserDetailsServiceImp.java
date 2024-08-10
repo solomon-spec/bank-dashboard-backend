66cb4ce340cad5aa4db0c1143c196ad87820b78b
solomon Abate <solomonabate18@gmail.com>
2024-08-10 13:46:00 +0300

    Add service layer

---
+++ src/main/java/com/a2sv/bankdashboard/service/UserDetailsServiceImp.java
@@ -0,0 +1,24 @@
+package com.a2sv.bankdashboard.service;
+
+
+import com.a2sv.bankdashboard.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImp implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    public UserDetailsServiceImp(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username)
+                .orElseThrow(()-> new UsernameNotFoundException("User not found"));
+    }
+}
