262f63a35949d0df5a37257b4c3ded310004429d
solomon Abate <solomonabate18@gmail.com>
2024-08-10 11:52:08 +0300

    Add repository

---
+++ src/main/java/com/a2sv/bankdashboard/dto/AuthenticationResponse.java
@@ -0,0 +1,21 @@
+package com.a2sv.bankdashboard.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    @JsonProperty("message")
+    private String message;
+
+}
