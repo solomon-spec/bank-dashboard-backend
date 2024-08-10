bbf37a47138ae86d10b45cc6dcdb22088a5ca963
solomon Abate <solomonabate18@gmail.com>
2024-08-10 09:53:13 +0300

    Initial commit

---
+++ src/main/java/com/a2sv/bankdashboard/BankDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.a2sv.bankdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankDashboardApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/a2sv/bankdashboard/model/Role.java
@@ -0,0 +1,5 @@
+package com.a2sv.bankdashboard.model;
+
+public enum Role {
+    USER
+}
