3ba232d6bb631e07c2357ca883dc4f96201de128
solomon Abate <solomonabate18@gmail.com>
2024-08-12 17:29:11 +0300

    Configure variables

--- src/main/java/com/a2sv/bankdashboard/config/SecurityConfig.java
+++ src/main/java/com/a2sv/bankdashboard/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -75,6 +76,12 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
     }
+
+
+//    @Bean
+//    public OidcUserService oidcUserService() {
+//        return new OidcUserService();
+//    }
 
 
 }
