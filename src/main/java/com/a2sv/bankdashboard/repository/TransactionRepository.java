301b733e0a0345c72a9e26eb4f9fa4a7d96a1370
solomon Abate <solomonabate18@gmail.com>
2024-08-19 11:32:37 +0300

    Clean: clean up

--- src/main/java/com/a2sv/bankdashboard/repository/TransactionRepository.java
+++ src/main/java/com/a2sv/bankdashboard/repository/TransactionRepository.java
@@ -11,8 +11,7 @@ import java.util.List;
 
 
 public interface TransactionRepository extends MongoRepository<Transaction, String> {
-    Page<Transaction> findBySender(User sender, Pageable pageable);
-    Page<Transaction> findByReceiver(User receiver, Pageable pageable);
+
     Page<Transaction> findBySenderOrReceiver(User sender, User receiver, Pageable pageable);
     Page<Transaction> findByTypeAndSenderOrReceiver(TransactionType type, User sender, User receiver, Pageable pageable);
     List<Transaction> findBySenderOrReceiver(User sender, User receiver);
