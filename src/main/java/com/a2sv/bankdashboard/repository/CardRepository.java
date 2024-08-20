a00a201bb33c19f93abedc5def4cba614a738ec3
solomon Abate <solomonabate18@gmail.com>
2024-08-20 12:16:36 +0300

    Clean up

--- src/main/java/com/a2sv/bankdashboard/repository/CardRepository.java
+++ src/main/java/com/a2sv/bankdashboard/repository/CardRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
 
 public interface CardRepository extends MongoRepository<Card,String> {
     Page<Card> findByUserId(String user_id, Pageable pageable);
