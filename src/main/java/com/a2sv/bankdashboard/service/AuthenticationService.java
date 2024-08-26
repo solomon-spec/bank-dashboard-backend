03902124bd5d80e08f6ace463176ff1eb07582f3
solomon Abate <solomonabate18@gmail.com>
2024-08-26 10:19:31 +0300

    Remove debug

--- src/main/java/com/a2sv/bankdashboard/service/AuthenticationService.java
+++ src/main/java/com/a2sv/bankdashboard/service/AuthenticationService.java
@@ -19,7 +19,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
